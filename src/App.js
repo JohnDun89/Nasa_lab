41173cb5d570a779b63d388bb2a80adc63904986
John Duncan <john@johnduncanphoto.com>
2018-01-16 13:54:01 +0000

    app defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import NasaContainer from './containers/NasaContainer.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <NasaContainer />
     );
   }
 }
