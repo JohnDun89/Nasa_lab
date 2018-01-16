71b907ab94fd90f1bd7be454ceed5835bf613a7c
John Duncan <john@johnduncanphoto.com>
2018-01-16 14:12:48 +0000

    added a consturctor function in NasaContainer

--- src/components/NasaSelector.js
+++ src/components/NasaSelector.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import NasaDisplay from './NasaDisplay.js'
 
 class NasaSelector extends React.Component {
 
+
+ render() {
+
+   return (
+     <select id="nasa-facts">
+
+     </select>
+   )
+ }
 }
 
 export default NasaSelector;
--- src/containers/NasaContainer.js
+++ src/containers/NasaContainer.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import NasaDisplay from './components/NasaDisplay.js'
+import NasaSelector from './components/NasaSelector.js'
 
 class NasaContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    
+  }
+
 
   render() {
     return(
